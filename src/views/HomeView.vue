23c3c0ef71825a53c0263d69af9d5aeadc0c514d
indahwulan24 <12050120353@students.uin-suska.ac.id>
2022-05-09 01:51:09 +0700

    add all dist :')

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script>
-</script>
-
-<template>
-  <div class="text-center">
-    <h1>Assalamu'alaikum Warahmatullahi Wabarakatuh</h1>
-    <h2>Silahkan pilih Juz, Info Surah, atau Ayat yang ingin dicari!</h2>
-  </div>
-</template>
