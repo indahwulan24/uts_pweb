55324fd23419b3e398b5dc3d859e8f98599e53f3
indahwulan24 <12050120353@students.uin-suska.ac.id>
2022-05-09 02:03:52 +0700

    changes route

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/uts/',
+  base: '/uts_pweb/',
   plugins: [vue()],
   resolve: {
     alias: {
